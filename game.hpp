410aeda0ce00a6b1d3585efc703ff3c851f33879
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-03-23 15:50:41 +0800

    raise the comment

--- game.hpp
+++ game.hpp
@@ -2,6 +2,20 @@
 
 #include <SDL2/SDL.h>
 
+/**
+ * 为兼容 Windows 而变得丑陋
+ *
+ * vcpkg 不直接提供 SDL2main
+ * SDL2.h 把 main 改名为 SDL_main 了
+ * 导致 MSVC 连接器找不到程序入口
+ *
+ * 手动链接会引发别的问题，比如跟默认库冲突
+ * (否则 vcpkg 作者肯定脑子有坑才会整出“手动链接”这种脱了裤子那啥的活)
+ */
+#ifdef main
+#undef main
+#endif
+
 #include "cosmos.hpp"
 #include "matter.hpp"
 #include "misc.hpp"
@@ -34,17 +48,3 @@
 
 #include "datum/flonum.hpp"
 #include "virtualization/filesystem/imgdb.hpp"
-
-/**
- * 为兼容 Windows 而变得丑陋
- *
- * vcpkg 不直接提供 SDL2main
- * SDL2.h 把 main 改名为 SDL_main 了
- * 导致 MSVC 连接器找不到程序入口
- *
- * 手动链接会引发别的问题，比如跟默认库冲突
- * (否则 vcpkg 作者肯定脑子有坑才会整出“手动链接”这种脱了裤子那啥的活)
- */
-#ifdef main
-#undef main
-#endif
