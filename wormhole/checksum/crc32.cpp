3479239b100a72d8419c6765f51c1a947b2aa7e6
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-06-07 17:47:48 +0800

    do accumulate checksum correctly

--- wormhole/checksum/crc32.cpp
+++ wormhole/checksum/crc32.cpp
@@ -3,12 +3,12 @@
 using namespace WarGrey::STEM;
 
 /*************************************************************************************************/
-static unsigned long crc_table[256];
+static uint32_t crc_table[256];
 static int crc_table_computed = 0;   
 
 static void make_crc_table() {
-    for (unsigned long n = 0; n < 256; n++) {
-        unsigned long c = n;
+    for (uint32_t n = 0; n < 256; n++) {
+        uint32_t c = n;
     
         for (size_t k = 0; k < 8; k++) {
             if (c & 1) {
@@ -24,8 +24,8 @@ static void make_crc_table() {
     crc_table_computed = 1;
 }
 
-static unsigned long update_crc(unsigned long crc, const uint8_t* message, size_t start, size_t end) {
-    unsigned long c = crc;
+static uint32_t update_crc(uint32_t crc, const uint8_t* message, size_t start, size_t end) {
+    uint32_t c = crc;
 
     /**
      * Update a running CRC with the bytes message[start...end-1]
@@ -46,21 +46,21 @@ static unsigned long update_crc(unsigned long crc, const uint8_t* message, size_
 }
 
 /*************************************************************************************************/
-unsigned long WarGrey::STEM::checksum_crc32(const uint8_t* message, size_t start, size_t end) {
+uint32_t WarGrey::STEM::checksum_crc32(const uint8_t* message, size_t start, size_t end) {
     return update_crc(0xFFFFFFFFL, message, start, end) ^ 0xFFFFFFFFL;
 }
 
-unsigned long WarGrey::STEM::checksum_crc32(unsigned long acc_crc, const uint8_t* message, size_t start, size_t end) {
-    return update_crc(~acc_crc, message, start, end) ^ 0xFFFFFFFFL;
+uint32_t WarGrey::STEM::checksum_crc32(uint32_t acc_crc, const uint8_t* message, size_t start, size_t end) {
+    return update_crc(acc_crc ^ 0xFFFFFFFF, message, start, end) ^ 0xFFFFFFFFL;
 }
 
-unsigned long  WarGrey::STEM::checksum_crc32(unsigned long* acc_crc, const uint8_t* message, size_t start, size_t end) {
-    unsigned long crc = 0UL;
+uint32_t WarGrey::STEM::checksum_crc32(uint32_t* acc_crc, const uint8_t* message, size_t start, size_t end) {
+    uint32_t crc = 0UL;
 
     if (acc_crc == nullptr) {
         crc = checksum_crc32(message, start, end);
     } else {
-        crc = (update_crc(~(*acc_crc), message, start, end) ^ 0xFFFFFFFFL);
+        crc = update_crc((*acc_crc) ^ 0xFFFFFFFF, message, start, end) ^ 0xFFFFFFFFL;
         (*acc_crc) = crc;
     }
 
