db7a8717525876a7e23de634f512e9c128e906c3
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-09-12 11:12:51 +0800

    make `TheBigBang` more useful

--- bang.cpp
+++ bang.cpp
@@ -3,20 +3,9 @@
 using namespace WarGrey::STEM;
 
 /*************************************************************************************************/
-static std::string the_name = "青少计算机科学";
-
-/*************************************************************************************************/
-void WarGrey::STEM::the_big_bang_name(const char* name) {
-    the_name = std::string(name);
-}
-    
-const char* WarGrey::STEM::the_big_bang_name() {
-    return the_name.c_str();
-}
-
-/*************************************************************************************************/
 void WarGrey::STEM::TheBigBang::load(float width, float height) {
-    this->title = this->insert(new Labellet(GameFont::Title(), this->title_color, "%s: %s", the_name.c_str(), this->name()));
+    this->title = this->insert(new Labellet(GameFont::Title(), this->title_color,
+        "%s: %s", this->_the_name.c_str(), this->name()));
 
     this->agent = this->insert(new Linkmon());
     this->agent->scale(-1.0F, 1.0F);
