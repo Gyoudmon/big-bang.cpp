1ed74a6aec4b8bc9cf777a8b31772e8ccd7fd014
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-11-25 20:25:47 +0800

    .

--- graphics/geometry.hpp
+++ graphics/geometry.hpp
@@ -5,5 +5,5 @@
 namespace WarGrey::STEM {
     __lambda__ SDL_RendererFlip game_scales_to_flip(float x_scale, float y_scale);
     __lambda__ void game_flip_to_signs(SDL_RendererFlip flip, float* hsgn = nullptr, float* vsgn = nullptr);
-    __lambda__ void game_flip_to_signs(SDL_RendererFlip flip, double* hsgn = nullptr, double* vsgn = nullptr);    
+    __lambda__ void game_flip_to_signs(SDL_RendererFlip flip, double* hsgn = nullptr, double* vsgn = nullptr);
 }
